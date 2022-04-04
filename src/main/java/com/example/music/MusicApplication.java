4f5ea22f4f1b5b55774c622d742b3a8489257df7
5201314_Zq <ya942371102@qq.com>
2022-04-04 22:47:29 +0800

    add MusicApplication for @MapprtScan

--- src/main/java/com/example/music/MusicApplication.java
+++ src/main/java/com/example/music/MusicApplication.java
@@ -1,9 +1,12 @@
 package com.example.music;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.music.dao")
 public class MusicApplication {
 
     public static void main(String[] args) {
