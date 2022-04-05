46b11f30c90487809e04562a62c937fe09d8ec5b
5201314_Zq <ya942371102@qq.com>
2022-04-05 17:46:13 +0800

    SingerController增加add接口

---
+++ src/main/java/com/example/music/controller/SingerController.java
@@ -0,0 +1,63 @@
+package com.example.music.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.music.domain.Singer;
+import com.example.music.service.SingerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/singer")
+public class SingerController {
+    @Autowired
+    private SingerService singerService;
+
+    /**
+     * 添加歌手
+     */
+    @RequestMapping("/add")
+    public Object addSinger(HttpServletRequest request){
+        JSONObject jsonObject = new JSONObject();
+        String name = request.getParameter("name").trim();
+        String sex = request.getParameter("sex").trim();
+        /*头像*/
+        String pic = request.getParameter("pic").trim();
+        String birth = request.getParameter("birth").trim();
+        String location = request.getParameter("location").trim();
+        String introduction = request.getParameter("introduction").trim();
+        /*日期转换*/
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthDate = new Date();
+        try {
+            birthDate = dateFormat.parse(birth);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //保存到歌手的对象中
+        Singer singer = new Singer();
+        singer.setName(name);
+        /*强制转换为byte*/
+        singer.setSex(new Byte(sex));
+        singer.setBirth(birthDate);
+        singer.setPic(pic);
+        singer.setLocation(location);
+        singer.setIntroduction(introduction);
+        boolean flag = singerService.insert(singer);
+        if (flag){ //保存成功
+            jsonObject.put("code",1);
+            jsonObject.put("msg","添加成功");
+            return jsonObject;
+        }
+        jsonObject.put("code",0);
+        jsonObject.put("msg","添加失败");
+        return jsonObject;
+    }
+}
